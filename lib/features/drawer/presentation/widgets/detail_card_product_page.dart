6f83031380e41715569a8f90f3b453c85e83f321
Begim <beq1m7@gmail.com>
2024-07-16 15:09:13 +0500

    last

--- lib/features/drawer/presentation/widgets/detail_card_product_page.dart
+++ lib/features/drawer/presentation/widgets/detail_card_product_page.dart
@@ -47,7 +47,8 @@ class DetailCardProductPage extends StatefulWidget {
 
 class _DetailCardProductPageState extends State<DetailCardProductPage> {
   int count = 0;
-  int optom = 0;
+  int optom = 2;
+  int git = false;
   bool isvisible = false;
 
   int? selectedIndex = -1;
