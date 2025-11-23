6ae8e7494f84feea999bab0884f582aafa8004aa
Sundetov Begim <7beq1m@gmail.com>
2025-11-23 23:17:37 +0500

    test

--- lib/src/feature/profile/data/presentation/widgets/language_widget.dart
+++ lib/src/feature/profile/data/presentation/widgets/language_widget.dart
@@ -6,10 +6,10 @@ import '../../../../../core/common/constants.dart';
 import '../../../../app/widgets/custom_back_button.dart';
 
 class LanguageWidget extends StatefulWidget {
-  const LanguageWidget({Key? key}) : super(key: key);
+  const LanguageWidget({super.key});
 
   @override
-  _LanguageWidgetState createState() => _LanguageWidgetState();
+  State<LanguageWidget> createState() => _LanguageWidgetState();
 }
 
 class _LanguageWidgetState extends State<LanguageWidget> {
@@ -28,115 +28,96 @@ class _LanguageWidgetState extends State<LanguageWidget> {
       appBar: AppBar(
         leading: Padding(
           padding: const EdgeInsets.only(left: 22.0),
-          child: CustomBackButton(onTap: () {
-            Navigator.pop(context);
-          }),
+          child: CustomBackButton(
+            onTap: () {
+              Navigator.pop(context);
+            },
+          ),
         ),
         elevation: 0,
         centerTitle: true,
         title: const Text(
           'Язык приложения',
           textAlign: TextAlign.center,
-          style: TextStyle(
-            fontSize: 16,
-            color: Colors.black,
-          ),
+          style: TextStyle(fontSize: 16, color: Colors.black),
         ),
         backgroundColor: Colors.white,
       ),
-      body: Column(children: [
-        const SizedBox(height: 15),
-        GestureDetector(
-          onTap: () {
-            index = 1;
-            _box.write('language_index', index);
-            setState(() {
-              index;
-            });
-          },
-          child: Container(
-            color: Colors.white,
-            padding: const EdgeInsets.only(left: 13, right: 13, top: 20),
-            width: MediaQuery.of(context).size.width,
-            child: Row(
-              children: [
-                SvgPicture.asset(
-                  'assets/temp/kaz.svg',
-                  height: 30,
-                  width: 30,
-                ),
-                const SizedBox(
-                  width: 20,
-                ),
-                SizedBox(
+      body: Column(
+        children: [
+          const SizedBox(height: 15),
+          GestureDetector(
+            onTap: () {
+              index = 1;
+              _box.write('language_index', index);
+              setState(() {
+                index;
+              });
+            },
+            child: Container(
+              color: Colors.white,
+              padding: const EdgeInsets.only(left: 13, right: 13, top: 20),
+              width: MediaQuery.of(context).size.width,
+              child: Row(
+                children: [
+                  SvgPicture.asset('assets/temp/kaz.svg', height: 30, width: 30),
+                  const SizedBox(width: 20),
+                  SizedBox(
                     width: 280,
                     child: Row(
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
                         const Text(
                           'Қазақша',
-                          style: TextStyle(
-                              fontSize: 16, fontWeight: FontWeight.w400),
+                          style: TextStyle(fontSize: 16, fontWeight: FontWeight.w400),
                         ),
                         index == 1
-                            ? const Icon(
-                                Icons.check,
-                                size: 20,
-                                color: AppColors.kPrimaryColor,
-                              )
-                            : Container()
+                            ? const Icon(Icons.check, size: 20, color: AppColors.kPrimaryColor)
+                            : Container(),
                       ],
-                    ))
-              ],
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
-        ),
-        GestureDetector(
-          onTap: () {
-            index = 2;
-            _box.write('language_index', index);
-            setState(() {
-              index;
-            });
-          },
-          child: Container(
-            color: Colors.white,
-            padding: const EdgeInsets.only(left: 13, right: 13, top: 20),
-            width: MediaQuery.of(context).size.width,
-            child: Row(
-              children: [
-                SvgPicture.asset(
-                  'assets/temp/rus.svg',
-                  height: 30,
-                  width: 30,
-                ),
-                const SizedBox(
-                  width: 20,
-                ),
-                SizedBox(
+          GestureDetector(
+            onTap: () {
+              index = 2;
+              _box.write('language_index', index);
+              setState(() {
+                index;
+              });
+            },
+            child: Container(
+              color: Colors.white,
+              padding: const EdgeInsets.only(left: 13, right: 13, top: 20),
+              width: MediaQuery.of(context).size.width,
+              child: Row(
+                children: [
+                  SvgPicture.asset('assets/temp/rus.svg', height: 30, width: 30),
+                  const SizedBox(width: 20),
+                  SizedBox(
                     width: 280,
                     child: Row(
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
                         const Text(
                           'Русский',
-                          style: TextStyle(
-                              fontSize: 16, fontWeight: FontWeight.w400),
+                          style: TextStyle(fontSize: 16, fontWeight: FontWeight.w400),
                         ),
                         index == 2
-                            ? const Icon(
-                                Icons.check,
-                                size: 20,
-                                color: AppColors.kPrimaryColor,
-                              )
-                            : Container()
+                            ? const Icon(Icons.check, size: 20, color: AppColors.kPrimaryColor)
+                            : Container(),
                       ],
-                    ))
-              ],
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
-        ),
-      ]),
+        ],
+      ),
     );
   }
 }
