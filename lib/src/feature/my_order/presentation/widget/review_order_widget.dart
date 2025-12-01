959159ac75c1780ca4d7e6d0f5d4f12bd9e2a847
Abylay <79087226+Aboneyx@users.noreply.github.com>
2025-12-01 23:56:46 +0500

    refactor: deleted old files

--- lib/src/feature/my_order/presentation/widget/review_order_widget.dart
+++ lib/src/feature/my_order/presentation/widget/review_order_widget.dart
@@ -37,7 +37,7 @@ class _ReviewOrderWidgetPageState extends State<ReviewOrderWidgetPage> {
 
   bool change = false;
 
-  TextEditingController _commentController = TextEditingController();
+  final TextEditingController _commentController = TextEditingController();
 
   List<String> cancel = [
     'Передумал покупать',
@@ -266,7 +266,7 @@ class _ReviewOrderWidgetPageState extends State<ReviewOrderWidgetPage> {
                           padding: EdgeInsets.zero,
                           physics: const ClampingScrollPhysics(),
                           itemCount: _image.length,
-                          separatorBuilder: (_, __) => const SizedBox(width: 8),
+                          separatorBuilder: (_, _) => const SizedBox(width: 8),
                           itemBuilder: (context, index) {
                             return SizedBox(
                               width: 64,
